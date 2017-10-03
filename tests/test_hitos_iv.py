7d6614087eb77258f3c0cb64ffc80ae7808d0058
lulivi <luislivilla@gmail.com>
2017-10-04 00:11:33 +0200

    Añadido test para funcion de numero de mielstones

--- tests/test_hitos_iv.py
+++ tests/test_hitos_iv.py
@@ -15,6 +15,12 @@ class HitosIvTest(unittest.TestCase):
         self.assertFalse(
             hitos_iv.read_json('../hitos.json') == {}, 'Diccionario no vacío')
 
+    def test_milestone_number(self):
+        """Comprueba si el numero de milestones es correcto."""
+        mil_dict = hitos_iv.read_json('../hitos.json')
+        self.assertEqual(
+            hitos_iv.milestone_number(mil_dict), 2, 'Número correcto de hitos')
+
 
 if __name__ == '__main__':
     unittest.main()
