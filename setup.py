4045301f810edd67f02f57948ce7bc28f7e61425
lulivi <luislivilla@gmail.com>
2017-10-10 18:00:00 +0200

    Another try? Deleted install_requires #6

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(
    name='hitos-iv',
    version='0.2',
    description='A JSON reader module',
-   license="GPLv3",
    long_description=long_description,
+   license="GPLv3",
    author='Luis Liñán',
    author_email='luislivilla@gmail.com',
    url="https://github.com/lulivi/hitos-iv",
-   install_requires=['unittest', 'json'],
 )
