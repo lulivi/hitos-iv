e7c9ac6e29d66deab2277740f9cd7054df236c95
lulivi <luislivilla@gmail.com>
2017-10-16 09:32:30 +0200

    Added install_requires

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@
 # external imports
 from setuptools import setup
 
+# long description
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+# requirements
+requirements = ['pocha']
+
 setup(
-   name='hitos-iv',
-   version='0.2',
-   description='A JSON reader module',
-   long_description=long_description,
-   license="GPLv3",
-   author='Luis Liñán',
-   author_email='luislivilla@gmail.com',
-   url="https://github.com/lulivi/hitos-iv",
-)
+    name='hitos-iv',
+    version='0.2',
+    description='A JSON reader module',
+    long_description=long_description,
+    license="GPLv3",
+    author='Luis Liñán',
+    author_email='luislivilla@gmail.com',
+    url="https://github.com/lulivi/hitos-iv",
+    install_requires=requirements, )
