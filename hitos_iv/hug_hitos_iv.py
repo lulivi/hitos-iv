decf36d8a6676edf584a478a302a6f9b839dd010
lulivi <luislivilla@gmail.com>
2017-11-06 11:47:36 +0100

    Added simple docs for hug functions

--- hitos_iv/hug_hitos_iv.py
+++ hitos_iv/hug_hitos_iv.py
@@ -6,24 +6,28 @@ json_path = '../data/hitos.json'
 hitos_iv_json = hitos_iv.read_json(json_path)
 
 
+# === Get `/` ===
 @hug.get('/')
 def index():
     """Retunr web status."""
     return {'status': 'OK'}
 
 
+# === Get `/all` ===
 @hug.get('/all')
 def get_all():
     """Return all milestones."""
     return hitos_iv.read_json(json_path)
 
 
+# === Get `/number` ===
 @hug.get('/number')
 def milestone_number():
     """Return all milestones."""
     return hitos_iv.milestone_number(hitos_iv_json)
 
 
+# === Get `/get/{id}` ===
 @hug.get('/get/{id}')
 def get_one(id: int):
     """Return one milestone."""
